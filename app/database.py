fdcfb02d70e830ea244023c887830214dc81c26d
vperizes <vperizes@gmail.com>
2024-07-08 23:13:25 +0300

    set up sqlalchemy orm

---
+++ app/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# create DB URL
+SQLALCHEMY_DATABASE_URL = f"postgresql://{os.getenv("USER")}:{os.getenv("PASS")}@{os.getenv("HOSTNAME")}/{os.getenv("DBNAME")}"
+
+# create a SQLAlchemy "engine"
+engine=create_engine(SQLALCHEMY_DATABASE_URL)
+
+#create instance of sessionlocal class
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# declartive_base() returns a class. we inherit from this class to create each of the db models or classes
+Base = declarative_base()
+
