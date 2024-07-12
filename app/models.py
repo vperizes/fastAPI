403fb87b511a292854da3e8e311a9991b399f62f
vperizes <vperizes@gmail.com>
2024-07-12 22:08:24 +0300

    update sqlalchemy post model

--- app/models.py
+++ app/models.py
@@ -8,6 +8,7 @@ from sqlalchemy import (
     Text,
 )
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql.expression import text
 from .database import Base
 
 
@@ -17,5 +18,7 @@ class Post(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(Text, nullable=False)
-    published = Column(Boolean, default=True)
-    created_at = Column(DateTime())
+    published = Column(Boolean, server_default="TRUE", nullable=False)
+    created_at = Column(
+        DateTime(timezone=True), nullable=False, server_default=text("now()")
+    )
