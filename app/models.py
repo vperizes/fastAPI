f0175538375a251e0c99d70ad1035da30d349e40
vperizes <vperizes@gmail.com>
2024-07-14 12:37:48 +0300

    add comments

--- app/models.py
+++ app/models.py
@@ -12,6 +12,7 @@ from sqlalchemy.sql.expression import text
 from .database import Base
 
 
+# this is sqlalchemy model (define what table looks like)
 class Post(Base):
     __tablename__ = "posts"
 
