13a8494edcd9d9c5d4143e81980e1e8d06392bde
vperizes <vperizes@gmail.com>
2024-08-04 16:06:59 -0500

    add password hashing to new user route

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ from typing import List
 
 from . import models, schemas
 from .database import engine, get_db
+from .passwordUtils import get_password
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -39,6 +40,11 @@ async def root():
 ###### USER ROUTES ######
 @app.post("/new-user", status_code=status.HTTP_201_CREATED, response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+
+    # hash password - user.password
+    hashed_password = get_password(user.password)
+    user.password = hashed_password
+
     new_user = models.User(**user.model_dump())
     db.add(new_user)
     db.commit()
---
+++ app/passwordUtils.py
@@ -0,0 +1,15 @@
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password(password: str):
+    return pwd_context.hash(password)
+
+
+def authenticate_user(db, username: str, password: str):
+    pass
