c0eaeeffd08c2107b9d8ba8d9b42c9ff9ab76658
vperizes <vperizes@gmail.com>
2024-08-21 16:31:04 -0500

    add query params to get all posts route

--- app/routes/postRouter.py
+++ app/routes/postRouter.py
@@ -1,6 +1,6 @@
 from fastapi import status, HTTPException, Response, Depends, APIRouter
 from sqlalchemy.orm import Session
-from typing import List
+from typing import List, Optional
 from .. import models, schemas, oauth
 from ..database import get_db
 
@@ -11,10 +11,10 @@ router = APIRouter(prefix="/posts", tags=["Posts"])
 
 
 @router.get("/", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth.get_current_user)):
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth.get_current_user), limit: int = 10, skip: int = 0, search: Optional[str] = ""):
     # cur.execute("SELECT * FROM posts")
     # all_posts = cur.fetchall()
-    all_posts = db.query(models.Post).all()
+    all_posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
     return all_posts
 
 
