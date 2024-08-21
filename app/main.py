8be308a62bb8dfe1b633a394a9ddd989bdae941d
vperizes <vperizes@gmail.com>
2024-08-21 16:36:20 -0500

    remove old db connection code using postgres driver

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
-import os
-import psycopg
-from psycopg.rows import dict_row
+
 
 from . import models
 from .database import engine
@@ -12,22 +10,6 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 load_dotenv()
-
-try:
-    conn = psycopg.connect(
-        host=os.getenv("HOST"),
-        dbname=os.getenv("DBNAME"),
-        user=os.getenv("USER"),
-        password=os.getenv("PASS"),
-        row_factory=dict_row,  # add row factory to return key:val pair
-    )
-
-    # Open a cursor to perform database operations
-    cur = conn.cursor()
-    print("Database connection was successful")
-except Exception as err:
-    print("Database connection failed")
-    print("Error: ", err)
 
 #### import routes
 app.include_router(authRouter.router)
