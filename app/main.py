5e3bae0590f28b953e94b8a4377aa3ef536a1e00
vperizes <vperizes@gmail.com>
2024-09-02 23:22:22 -0500

    set up cors

--- app/main.py
+++ app/main.py
@@ -1,13 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 from . import models
 from .database import engine
 from .routes import postRouter, userRouter, authRouter, voteRouter
 
-models.Base.metadata.create_all(bind=engine)
+# comment out create tables since alembic is set up
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 load_dotenv()
+
+#### CORS setup
+origins = ["*"] #setting origin to all domains for testing purposes
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 
 #### import routes
 app.include_router(authRouter.router)
